45bec0c2e031592e9e335cdfa9afc1d6587d9eaa
Nishant Raj <nishantraj3051@gmail.com>
2023-07-23 21:25:08 +0530

    first commit

---
+++ backend/server1.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const expressApp = express();
+const httpServer = require('http').createServer(expressApp);
+
+const io = require('socket.io')(httpServer, {
+  cors: { origin: true },
+});
+
+const port = process.env.PORT || 8000;
+
+httpServer.listen(port, () => {
+  console.log('Server running at', port);
+});
+
+io.on('connection', (socket) => {
+  console.log('user online');
+  socket.on('image-data', (data) => {
+    socket.broadcast.emit('image-data', data);
+  });
+});
